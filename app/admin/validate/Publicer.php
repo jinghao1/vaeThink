825b4f537d739700c7f309619e998de6de2686bb
sjh <sjh@jindong-auto.com>
2019-09-30 15:52:44 +0800

    var think

--- app/admin/validate/Publicer.php
+++ app/admin/validate/Publicer.php
@@ -17,13 +17,13 @@ class Publicer extends Validate
     protected $rule = [
         'username'       => 'require',
         'password'       => 'require',
-        'captcha'        => 'require|captcha',
+        //'captcha'        => 'require|captcha',
     ];
 
     protected $message = [
         'username.require'       => '用户名不能为空',
         'password.require'       => '密码不能为空',
-        'captcha.require'        => '验证码不能为空',
-        'captcha.captcha'        => '验证码不正确',
+        //'captcha.require'        => '验证码不能为空',
+        //'captcha.captcha'        => '验证码不正确',
     ];
 }
